c14006f85f66a77e8057e7d39b11a2f7886d4599
yoon-junseo <ysj6844@naver.com>
2021-11-22 01:35:18 +0900

    Feature: App 수정
    1. 전역 스타일 적용
    2. theme 제공
    #1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
 import Router from '@/Router';
 
+import AppLayout from '@/components/common/AppLayout';
+
+import { GlobalStyles } from '@/theme/GlobalStyle';
+import { Theme } from '@/theme/Theme';
+
 const App = () => {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={Theme}>
+        <AppLayout>
+          <Router />
+        </AppLayout>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
