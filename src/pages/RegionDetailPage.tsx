6040e001bbfe60fdbb36172d0fcc43ea16bb0a46
yoon-junseo <ysj6844@naver.com>
2021-11-22 00:17:17 +0900

    Feature: 페이지 생성
    #2

---
+++ src/pages/RegionDetailPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegionDetailPage = () => {
+  return <div>RegionDetailPage</div>;
+};
+
+export default RegionDetailPage;
---
+++ src/pages/TripListPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TripListPage = () => {
+  return <div>TripListPage</div>;
+};
+
+export default TripListPage;
