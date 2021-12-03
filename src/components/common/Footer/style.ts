0cddeb516a5e05395d61ca7eba102bb0940ace8d
yoon-junseo <ysj6844@naver.com>
2021-12-04 01:10:34 +0900

    Style: Footer
    1. 최소 넓이 추가
    #1

--- src/components/common/Footer/style.ts
+++ src/components/common/Footer/style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.footer`
   width: 100%;
   height: 161px;
+  min-width: 320px;
 
   background-color: ${({ theme }) => theme.B_2};
 `;
