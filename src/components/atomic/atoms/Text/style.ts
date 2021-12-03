63a6b618836a31f7585e698164f5185545402c7c
yoon-junseo <ysj6844@naver.com>
2021-12-04 01:12:01 +0900

    Feature: Text 컴포넌트
    1. 클릭이 가능한 span으로 구성된 컴포넌트
    2. 스타일
    #1

---
+++ src/components/atomic/atoms/Text/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import { ITextProps } from '@/components/atomic/atoms/Text';
+
+export const StyledSpan = styled.span<ITextProps>`
+  cursor: ${(props) => (props.cursor ? 'pointer' : '')};
+
+  color: ${(props) => props.fontColor};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+`;
