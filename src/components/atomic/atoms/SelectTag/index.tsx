d1017c751b71025fc14c522d19b1cb3c533c9634
yoon-junseo <ysj6844@naver.com>
2021-12-04 18:34:25 +0900

    Fix: SelectTag
    1. checkedValue를 props로 받아와서  isSelected를 내부에서 결정
    2. margin props 추가, defaut = 0px
    #1

--- src/components/atomic/atoms/SelectTag/index.tsx
+++ src/components/atomic/atoms/SelectTag/index.tsx
@@ -1,33 +1,57 @@
-import React, { MouseEvent, ReactText } from 'react';
+import React, { useState, useEffect, MouseEvent, ReactText } from 'react';
 import styled from 'styled-components';
 
 import { Theme } from '@/theme/Theme';
 
 interface ISelectTagProps {
   onClick?: (e: MouseEvent<HTMLSpanElement>) => void;
+  checkedValue: string | null;
+}
+
+interface ISelectTagStyledProps {
   isSelected: boolean;
+  margin: string;
 }
 
 interface ISelectTagComponentProps extends ISelectTagProps {
   children?: ReactText;
+  margin: string;
 }
 
-const Wrapper = styled.span<ISelectTagProps>`
+const Wrapper = styled.span<ISelectTagStyledProps>`
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
 
-  width: 190px;
+  max-width: 100px;
+  width: 100%;
+
+  ${({ theme }) => theme.desktop} {
+    min-width: 190px;
+  }
   height: 36px;
+
+  color: ${(props) => (props.isSelected ? Theme.F_4 : '#707070')};
+
+  margin: ${(props) => props.margin};
   border-radius: 32px;
-  border: ${(props) => (props.isSelected ? Theme.M_1 : Theme.F_3)};
+  border: ${(props) => (props.isSelected ? `1px solid ${Theme.M_1}` : `1px solid ${Theme.F_3}`)};
   background-color: ${(props) => (props.isSelected ? Theme.F_1 : Theme.B_2)};
 `;
 
-const SelectTag = ({ children, onClick, isSelected }: ISelectTagComponentProps) => {
+const SelectTag = ({ children, onClick, checkedValue, margin = '0px' }: ISelectTagComponentProps) => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  useEffect(() => {
+    if (checkedValue === children) {
+      setIsSelected(true);
+    } else {
+      setIsSelected(false);
+    }
+  });
+
   return (
-    <Wrapper onClick={onClick} isSelected={isSelected}>
+    <Wrapper onClick={onClick} isSelected={isSelected} margin={margin}>
       {children}
     </Wrapper>
   );
