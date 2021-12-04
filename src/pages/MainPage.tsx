862ebec606b3d99a318605f318467b72fe8d328e
yoon-junseo <ysj6844@naver.com>
2021-12-04 13:32:40 +0900

    Feature: 페이지 사용 방법 변경
    1. AppLaytout 삽입
    2. 다른 컴포넌트 테스트
    #1

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import AppLayout from '@/components/common/AppLayout';
+
+import LeftRadiusBox from '@/components/atomic/atoms/LeftRadiusBox';
+
+import LeftRadiusContents from '@/components/atomic/molecules/LeftRadiusContents';
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <AppLayout>
+      MainPage
+      <LeftRadiusBox width="17px" height="55px" backgroundColor="#FF7F8C" />
+      <LeftRadiusContents width="115px" height="55px">
+        숙소
+      </LeftRadiusContents>
+    </AppLayout>
+  );
 };
 
 export default MainPage;
