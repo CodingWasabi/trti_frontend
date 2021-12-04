27c30155fd5181ae0955bd8730b53e42654379b6
yoon-junseo <ysj6844@naver.com>
2021-12-04 18:36:49 +0900

    Feature: 생성된 컴포넌트들 사용 방법
    #1

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,16 +4,21 @@ import AppLayout from '@/components/common/AppLayout';
 
 import LeftRadiusBox from '@/components/atomic/atoms/LeftRadiusBox';
 
-import LeftRadiusContents from '@/components/atomic/molecules/LeftRadiusContents';
+import TreatyContentsType from '@/components/atomic/molecules/TreatyContentsType';
+
+import TreatyContentsList from '@/components/atomic/molecules/TreatyContentsList';
+import TreatyContents from '@/components/atomic/organisms/TreatyContents';
 
 const MainPage = () => {
   return (
     <AppLayout>
       MainPage
       <LeftRadiusBox width="17px" height="55px" backgroundColor="#FF7F8C" />
-      <LeftRadiusContents width="115px" height="55px">
+      <TreatyContentsType width="115px" height="55px">
         숙소
-      </LeftRadiusContents>
+      </TreatyContentsType>
+      <TreatyContentsList />
+      <TreatyContents />
     </AppLayout>
   );
 };
