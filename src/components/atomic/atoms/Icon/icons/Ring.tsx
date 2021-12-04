9134ccb966708d959249e1ddec4d63feec61bb6a
yoon-junseo <ysj6844@naver.com>
2021-12-04 18:30:29 +0900

    Feature: RingIcon 추가
    #1

---
+++ src/components/atomic/atoms/Icon/icons/Ring.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Ring = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="6" viewBox="0 0 15 6" fill="none">
+      <path
+        d="M14 5C14 2.79086 11.0899 1 7.5 1C3.91015 1 1 2.79086 1 5"
+        stroke="#707070"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default Ring;
