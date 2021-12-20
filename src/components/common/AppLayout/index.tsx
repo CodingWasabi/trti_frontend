0d91ce3f17381a3e840379ed81a1010e351272f5
yoon-junseo <ysj6844@naver.com>
2021-12-21 00:56:14 +0900

    fix: AppLayout
    1. PlusButton 추가
    2. PlusButton 위치 조정을 위한 작업
    #11

--- src/components/common/AppLayout/index.tsx
+++ src/components/common/AppLayout/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import useGetResponsive from '@/hooks/useGetResponsive';
 
 import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 
+import PlusButton from '@/components/atomic/atoms/PlusButton';
+
 import NavBar from '@/components/atomic/molecules/NavBar';
 
-import { Wrapper, ContentsWrapper, Contents } from './style';
+import { Wrapper, ContentsWrapper, Contents, PlusButtonWrapper } from './style';
 
 interface IAppLayoutComponentProps {
   children: React.ReactNode;
@@ -22,6 +24,9 @@ const AppLayout = ({ children }: IAppLayoutComponentProps) => {
       <ContentsWrapper>
         <Contents>{children}</Contents>
       </ContentsWrapper>
+      <PlusButtonWrapper width={width} height={height}>
+        <PlusButton />
+      </PlusButtonWrapper>
       <Footer />
     </Wrapper>
   );
--- src/components/common/AppLayout/style.ts
+++ src/components/common/AppLayout/style.ts
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
 
+import useGetClientRect from '@/hooks/useGetClientRect';
+
+const { width, height } = useGetClientRect();
+
+interface IPlusButtonWrapper {
+  width: number | undefined;
+  height: number | undefined;
+}
+
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  width: 100%;
 `;
 
 export const ContentsWrapper = styled.div`
@@ -22,8 +32,13 @@ export const Contents = styled.div`
   min-width: 320px;
   width: 100%;
   min-height: 100%;
-
   ${({ theme }) => theme.desktop} {
-    width: 790px;
+    max-width: 790px;
   }
+`;
+
+export const PlusButtonWrapper = styled.div<IPlusButtonWrapper>`
+  position: absolute;
+  top: ${height - 100}px;
+  left: ${width - 250}px;
 `;
