15d2d454977892c2c3dbe2d59343673bc390bea9
yoon-junseo <ysj6844@naver.com>
2021-12-22 16:18:04 +0900

    fix: NavBar
    1. NavBar에 Link 추가하고, onClick 제거
    2. Text를 제외하고 li로 변경
    #14

--- src/components/atomic/molecules/NavBar/index.tsx
+++ src/components/atomic/molecules/NavBar/index.tsx
@@ -1,47 +1,24 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import Text from '@/components/atomic/atoms/Text';
-
-import { Theme } from '@/theme/Theme';
-
-import { Wrapper, NavBarBlock } from './style';
+import { Wrapper, NavBarBlock, StyledLi } from './style';
 
 const NavBar = () => {
-  const history = useHistory();
-
-  const onClickTendency = () => {
-    console.log('coi');
-    history.push('/tendency');
-  };
-
-  const onClickRegions = () => {
-    history.push('/regions');
-  };
-
-  const onClickTrips = () => {
-    history.push('/trips');
-  };
-
-  const onClickMypage = () => {
-    history.push('/mypage');
-  };
-
   return (
     <Wrapper>
       <NavBarBlock>
-        <Text fontColor={Theme.F_4} fontSize="16px" fontWeight="500" cursor={true} onClick={onClickTendency}>
-          여행성향
-        </Text>
-        <Text fontColor={Theme.F_4} fontSize="16px" fontWeight="500" cursor={true} onClick={onClickRegions}>
-          여행지
-        </Text>
-        <Text fontColor={Theme.F_4} fontSize="16px" fontWeight="500" cursor={true} onClick={onClickTrips}>
-          나의 여행
-        </Text>
-        <Text fontColor={Theme.F_4} fontSize="16px" fontWeight="500" cursor={true} onClick={onClickMypage}>
-          개인설정
-        </Text>
+        <Link to="/tendency">
+          <StyledLi>여행성향</StyledLi>
+        </Link>
+        <Link to="/regions">
+          <StyledLi>여행지</StyledLi>
+        </Link>
+        <Link to="/trips">
+          <StyledLi>나의 여행</StyledLi>
+        </Link>
+        <Link to="/mypage">
+          <StyledLi>개인설정</StyledLi>
+        </Link>
       </NavBarBlock>
     </Wrapper>
   );
--- src/components/atomic/molecules/NavBar/style.ts
+++ src/components/atomic/molecules/NavBar/style.ts
@@ -11,14 +11,16 @@ export const Wrapper = styled.div`
 
   height: 50px;
 
-  border-top: 1px solid ${Theme.F_1};
-  border-bottom: 1px solid ${Theme.F_1};
+  border-top: 1px solid ${({ theme }) => theme.F_1};
+  border-bottom: 1px solid ${({ theme }) => theme.F_1};
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
 `;
 
-export const NavBarBlock = styled.div`
+export const NavBarBlock = styled.ul`
   display: flex;
   justify-content: space-between;
+
+  list-style: none;
 
   padding: 0px 15px;
 
@@ -28,4 +30,10 @@ export const NavBarBlock = styled.div`
   ${({ theme }) => theme.desktop} {
     max-width: 750px;
   }
+`;
+
+export const StyledLi = styled.li`
+  color: ${({ theme }) => theme.F_4};
+  font-size: 16px;
+  font-weight: 500;
 `;
