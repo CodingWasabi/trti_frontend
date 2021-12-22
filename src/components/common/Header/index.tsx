169a6314073b0d2d59232fafd0b3388c3d6094f1
yoon-junseo <ysj6844@naver.com>
2021-12-22 16:21:36 +0900

    fix: Header
    1. Burger, LeftNav 스타일 추가
    2. 헤더 수정 (알람, 유저 아이콘으로 변경)
    #14

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Icon from '@/components/atomic/atoms/Icon';
+
+import Burger from '@/components/common/Header/Burger';
+
 import { Wrapper, HeaderBlock, Right, Login } from '@/components/common/Header/style';
 
 const Header = () => {
   return (
     <Wrapper>
       <HeaderBlock>
+        <Burger />
         <a href="/">
-          <Icon icon="Logo" />
+          <Icon icon="Logo" width={100} height={20} />
         </a>
         <Right>
-          <Login>
-            <a href="/login">로그인</a>
-          </Login>
-          <Icon icon="ProfileIcon" />
+          <Icon icon="Alarm" />
+          <Link to="/mypage">
+            <Icon icon="User" />
+          </Link>
         </Right>
       </HeaderBlock>
     </Wrapper>
--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
+import { IBurger } from './Burger';
+
 export const Wrapper = styled.div`
-  position: static;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -28,7 +29,7 @@ export const Right = styled.div`
   align-items: center;
   justify-content: space-between;
 
-  width: 120px;
+  width: 60px;
 `;
 
 export const Login = styled.span`
@@ -47,5 +48,75 @@ export const Login = styled.span`
 
   a {
     color: ${({ theme }) => theme.F_3};
+  }
+`;
+
+export const StyledBurger = styled.div<IBurger>`
+  cursor: pointer;
+
+  z-index: 1001;
+  position: ${({ open }) => (open ? 'fixed' : '')};
+
+  display: flex;
+  justify-content: space-around;
+  flex-flow: column nowrap;
+
+  width: 32px;
+  height: 32px;
+
+  ${({ theme }) => theme.desktop} {
+    display: none;
+  }
+
+  div {
+    width: 32px;
+    height: 4px;
+
+    background-color: ${({ open }) => (open ? '#ccc' : '#333')};
+    border-radius: 10px;
+
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+
+    &:nth-child(1) {
+      transform: ${({ open }) => (open ? 'rotate(45deg)' : 'rotate(0)')};
+    }
+    &:nth-child(2) {
+      transform: ${({ open }) => (open ? 'translateX(100%)' : 'translateX(0)')};
+      opacity: ${({ open }) => (open ? 0 : 1)};
+    }
+    &:nth-child(3) {
+      transform: ${({ open }) => (open ? 'rotate(-45deg)' : 'rotate(0)')};
+    }
+  }
+`;
+
+export const StyledUl = styled.ul<IBurger>`
+  li {
+    padding: 18px 10px;
+    color: ${({ theme }) => theme.F_4};
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  flex-flow: column nowrap;
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+
+  width: 300px;
+  height: 100vh;
+
+  padding-top: 3.5rem;
+  background-color: pink;
+
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
+  transition: transform 0.3s ease-in-out;
+
+  ${({ theme }) => theme.desktop} {
+    list-style: none;
+    display: flex;
+    flex-flow: row nowrap;
   }
 `;
