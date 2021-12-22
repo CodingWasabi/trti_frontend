5dea84e389e575c6719a59301d104daa6bde72c2
yoon-junseo <ysj6844@naver.com>
2021-12-22 16:21:50 +0900

    fix: MainPage
    1. MainPage에 테스트 컴포넌트 삭제
    #14

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,25 +2,8 @@ import React from 'react';
 
 import AppLayout from '@/components/common/AppLayout';
 
-import LeftRadiusBox from '@/components/atomic/atoms/LeftRadiusBox';
-
-import TreatyContentsType from '@/components/atomic/molecules/TreatyContentsType';
-
-import TreatyContentsList from '@/components/atomic/molecules/TreatyContentsList';
-import TreatyContents from '@/components/atomic/organisms/TreatyContents';
-
 const MainPage = () => {
-  return (
-    <AppLayout>
-      MainPage
-      <LeftRadiusBox width="17px" height="55px" backgroundColor="#FF7F8C" />
-      <TreatyContentsType width="115px" height="55px">
-        숙소
-      </TreatyContentsType>
-      <TreatyContentsList />
-      <TreatyContents />
-    </AppLayout>
-  );
+  return <AppLayout>MainPage</AppLayout>;
 };
 
 export default MainPage;
