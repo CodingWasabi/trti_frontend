601fcccfe5d79ff86e40ecf43050804c7ff9136d
yoon-junseo <ysj6844@naver.com>
2021-12-22 14:48:15 +0900

    fix: Icon
    1. width, height 추가
    #14

--- src/components/atomic/atoms/Icon/index.tsx
+++ src/components/atomic/atoms/Icon/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import * as icons from '@/components/atomic/atoms/Icon/icons';
+
+const IconWrapper = styled.div<{ width: number; height: number }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+`;
 
 type IconOption = keyof typeof icons;
 
 interface IIconProps {
   icon: IconOption;
+  width?: number;
+  height?: number;
 }
 
-const Icon = ({ icon }: IIconProps) => {
+const Icon = ({ icon, width, height }: IIconProps) => {
   const IconComponent = icons[icon];
-  return <IconComponent />;
+  return (
+    <IconWrapper width={width || 24} height={height || 24}>
+      <IconComponent />
+    </IconWrapper>
+  );
 };
 
 export default Icon;
