2b36aa93f47927fda4b0593a218253b08a7585db
yoon-junseo <ysj6844@naver.com>
2021-12-23 23:05:18 +0900

    fix: GroupInfoSelectRegion
    1. GroupInfoBox의 컴포넌트명 변경
    #16

--- src/components/atomic/molecules/GroupInfoSelectRegion/index.tsx
+++ src/components/atomic/molecules/GroupInfoSelectRegion/index.tsx
@@ -4,7 +4,7 @@ import { regions } from '@/lib/constants';
 
 import Select from '@/components/atomic/atoms/Select';
 
-import GroupInfoInput from '@/components/atomic/molecules/GroupInfoInput';
+import GroupInfoBox from '@/components/atomic/molecules/GroupInfoBox';
 
 interface IGroupInfoSelectRegion {
   onChangeSelect: (e: React.ChangeEvent<HTMLSelectElement>) => void;
@@ -12,9 +12,9 @@ interface IGroupInfoSelectRegion {
 
 const GroupInfoSelectRegion = ({ onChangeSelect }: IGroupInfoSelectRegion) => {
   return (
-    <GroupInfoInput icon="Marker">
+    <GroupInfoBox icon="Marker">
       <Select defaultValue="여행지를 선택하세요" onChangeSelect={onChangeSelect} selectOptions={regions} />
-    </GroupInfoInput>
+    </GroupInfoBox>
   );
 };
 
