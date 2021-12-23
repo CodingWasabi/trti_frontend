aad7a95b81cd6879c3622b4e10434ff352a1b150
yoon-junseo <ysj6844@naver.com>
2021-12-23 22:43:15 +0900

    style: fix css
    1. width, height가 number 타입이라서 뒤에 px 추가
    #16

--- src/components/atomic/atoms/LeftRadiusBox/style.ts
+++ src/components/atomic/atoms/LeftRadiusBox/style.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 import { ILeftRadiusBoxProps } from '@/components/atomic/atoms/LeftRadiusBox';
 
 export const LeftRadiusBoxContents = styled.div<ILeftRadiusBoxProps>`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
 
   background-color: ${(props) => props.backgroundColor};
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
