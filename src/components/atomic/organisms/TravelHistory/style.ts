7e52011299394918683270fb2c92ef6bf5ac4193
yoon-junseo <ysj6844@naver.com>
2021-12-25 19:51:09 +0900

    design: travelHistory
    1. 가운데 정렬 추가
    2. 최소 넓이 추가
    #11

--- src/components/atomic/organisms/TravelHistory/style.ts
+++ src/components/atomic/organisms/TravelHistory/style.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  min-width: 120px;
+`;
 
 export const ImageWrapper = styled.div``;
 
