e1d74f338a53c02df8f57ef0e44764fdb14431f9
yoon-junseo <ysj6844@naver.com>
2021-12-25 19:53:43 +0900

    feat: mainPage
    1. TravelHistory 추가
    2. PlusButton 추가
    #11

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import PlusButton from '@/components/atomic/atoms/PlusButton';
+
+import TravelHistoryList from '@/components/atomic/organisms/TravelHistoryList';
+
 import AppLayout from '@/components/common/AppLayout';
 
+import { travelHistoryList } from '@/mock';
+
 const MainPage = () => {
-  return <AppLayout>MainPage</AppLayout>;
+  return (
+    <AppLayout>
+      <TravelHistoryList travelHistoryList={travelHistoryList} />
+      <PlusButton />
+    </AppLayout>
+  );
 };
 
 export default MainPage;
