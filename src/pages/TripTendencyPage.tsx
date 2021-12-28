7b6f09519bca336c85935ade8edd7e7e82861fb4
yoon-junseo <ysj6844@naver.com>
2021-12-28 17:58:55 +0900

    fix: tripTendencyPage
    1. 여행성향 페이지 구성 변경
    #21

--- src/pages/TripTendencyPage.tsx
+++ src/pages/TripTendencyPage.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import ProgressBar from '@/components/atomic/atoms/ProgressBar';
 
 import QuestionSelection from '@/components/atomic/organisms/QuestionSelection';
 
 import AppLayout from '@/components/common/AppLayout';
 
+import { IAnswerProps } from '@/types/survey';
+
 import { questionList } from '@/mock';
 
 const TripTendencyPage = () => {
+  const [answers, setAnswers] = useState<Array<IAnswerProps>>([]);
+  const [clickedCount, setClickedCount] = useState<number>(0);
+  const [percentage, setPercentage] = useState<number>(0);
+
+  useEffect(() => {
+    setPercentage((clickedCount / questionList.length) * 100);
+  }, [clickedCount]);
+
   return (
-    <AppLayout>
-      <QuestionSelection questionList={questionList} />
+    <AppLayout isBlurred={true}>
+      <ProgressBar percentage={percentage} />
+      <QuestionSelection
+        questionList={questionList}
+        clickedCount={clickedCount}
+        answers={answers}
+        setClickedCount={setClickedCount}
+        setAnswers={setAnswers}
+      />
     </AppLayout>
   );
 };
