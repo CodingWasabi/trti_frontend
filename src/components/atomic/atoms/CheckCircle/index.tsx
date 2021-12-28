a293284d4379442c6b05d6615a265034066e31e4
yoon-junseo <ysj6844@naver.com>
2021-12-29 04:40:12 +0900

    fix: checkCircle
    1. 사이즈 조절 가능하게 수정
    2. 색상 변경 가능하게 수정
    #23

--- src/components/atomic/atoms/CheckCircle/index.tsx
+++ src/components/atomic/atoms/CheckCircle/index.tsx
@@ -2,12 +2,30 @@ import React from 'react';
 
 import Icon from '@/components/atomic/atoms/Icon';
 
+import { Theme } from '@/theme/Theme';
+
 import { Wrapper } from './style';
 
-const CheckCircle = () => {
+export interface ICheckCircleProps {
+  color?: string;
+  iconWidth?: number;
+  iconHeight?: number;
+  width?: string;
+  height?: string;
+  backgroundColor?: string;
+}
+
+const CheckCircle = ({
+  color = 'white',
+  width = '30px',
+  height = '30px',
+  iconWidth = 24,
+  iconHeight = 16,
+  backgroundColor = `${Theme.M_1}`,
+}: ICheckCircleProps) => {
   return (
-    <Wrapper>
-      <Icon icon="Check" width={24} height={16} />
+    <Wrapper color={color} width={width} height={height} backgroundColor={backgroundColor}>
+      <Icon icon="Check" width={iconWidth} height={iconHeight} />
     </Wrapper>
   );
 };
