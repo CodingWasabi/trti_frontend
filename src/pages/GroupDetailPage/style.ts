1ad3684a54595fa709a6cdb6fae685870acc3c27
yoon-junseo <ysj6844@naver.com>
2021-12-29 04:52:48 +0900

    feat: groupDetailPage
    1. 단일 파일 -> 폴더 / 파일 구조
    2. 화면 구성
    #23

---
+++ src/pages/GroupDetailPage/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)`
+  max-width: 375px;
+  width: 100%;
+  margin: 20px 0;
+`;
+
+export const SuggestionWrapper = styled.div`
+  width: 100%;
+  max-width: 500px;
+`;
