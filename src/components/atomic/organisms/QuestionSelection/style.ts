66b1b656a6bd4cd19db7ba11c68bd859b9a40d86
yoon-junseo <ysj6844@naver.com>
2021-12-28 17:57:01 +0900

    fix: questionSelection
    1. 라이브러리 사용한 코드 제거
    2. 캐러셀 기능 구현
    + 추후 함수 부분 리팩토링 필요
    #21

--- src/components/atomic/organisms/QuestionSelection/style.ts
+++ src/components/atomic/organisms/QuestionSelection/style.ts
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
-import Slider from 'react-slick';
 
-export const Wrapper = styled.div`
-  & div {
-    margin-bottom: 20px;
-  }
-`;
+export const QuestionSelectionWrapper = styled.div`
+  overflow: hidden;
 
-export const StyledSlider = styled(Slider)`
   width: 300px;
-  height: 300px;
+
+  margin: 30px 0px;
+  /* min-width: 300px;
+  max-width: 300px;
+  width: 100%; */
 `;
+
+export const SliderWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const ContentsWrapper = styled.div``;
 
 export const Row = styled.div`
   display: flex;
