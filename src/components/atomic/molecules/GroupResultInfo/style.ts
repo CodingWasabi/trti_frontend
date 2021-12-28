192df090baf4674072d42cbb1d2b951f0663eb52
yoon-junseo <ysj6844@naver.com>
2021-12-29 04:44:42 +0900

    feat: add GroupResultInfo
    1. 그룹 결과의 정보를 나타내는 컴포넌트
    #23

---
+++ src/components/atomic/molecules/GroupResultInfo/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 500px;
+  width: 100%;
+`;
+
+export const ContentsWrapper = styled.div`
+  position: relative;
+  top: -130px;
+`;
